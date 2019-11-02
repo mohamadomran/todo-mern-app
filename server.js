f0b0f0739c24cedb97a5563e2d220fec1caaebd4
Mohamad Omran <mohamad1_3omran@hotmail.com>
2019-11-02 02:31:57 +0200

    production small script in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-
+const path = require("path");
 const app = express();
 
 // Bodyparser Middleware
@@ -24,6 +24,15 @@ mongoose
 app.use("/api/todos", require("./routes/api/todos"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html')
+  });
+}
 
 const port = process.env.PORT || 5000;
 
